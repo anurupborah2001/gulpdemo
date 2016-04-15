d690057b8d0d5fb0ff501702f263ef3a02d28c23
Anurup <Anurup>
2016-04-15 15:32:31 +0800

    Compass Tutorial

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,8 @@ var gulp = require('gulp'),
     gulputil = require('gulp-util'),
     gulpcoffee = require('gulp-coffee'),
     concat = require('gulp-concat'),
-    browserify = require('gulp-browserify');
+    browserify = require('gulp-browserify'),
+    compass= require('gulp-compass');
 
 //Assign Variable
 var build = "builds/",
@@ -17,17 +18,35 @@ var build = "builds/",
     dest =  build + "production/",
     components = "components/",
     coffeePath =  components + "coffee/",
+    imageSource = source + "images/",
+    imageDest = dest + + "images/",
+    cssSource =  source + "css/",
+    cssDest = dest + + "css/",
+    jsSource = source + "js/",
+    jsDest =  dest + "js/",
+    saas =  components + "sass/",
     jsPath = components + "scripts/",
+    devBuild = ((process.env.NODE_ENV || 'development').trim().toLowerCase()!=='production')
     coffee = { 
         in : coffeePath,
         coffeeAll : coffeePath + "*.coffee",
         coffeeSources : [coffeePath + 'tagline.coffee'] 
     },
-    saas =  components + "sass/",
+    css = {
+        in : saas + "style.scss",
+        out : (!devBuild) ? cssDest : cssSource,
+        compassOpts : 
+           { 
+                sass : saas,
+                image : imageSource,
+                style : 'expanded'
+        }
+    },
+   
     js = {
         in : jsPath,
-        source : source + "js/",
-        dest : dest + "js/",
+        source : jsSource,
+        dest : jsDest,
         jsSources : [
                         jsPath + "rclick.js",
                         jsPath + "pixgrid.js",
@@ -44,6 +63,11 @@ gulp.task('js',function(){
     gulp.src(js.jsSources).pipe(concat('script.js')).pipe(browserify()).pipe(gulp.dest(js.source));
 });
 
-gulp.task('default',['coffee','js'],function(){
+gulp.task('compass',function(){
+    gulp.src(css.in).pipe(compass(css.compassOpts)).on('error',gulputil.log).pipe(gulp.dest(css.out));
+});
+
+
+gulp.task('default',['coffee','js','compass'],function(){
     
 })
