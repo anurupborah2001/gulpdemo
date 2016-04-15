af35dee79925da619fd8e6527d29e71d2523be13
Anurup <Anurup>
2016-04-15 11:22:28 +0800

    Gulp and GulpUtil

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ * GULP Tutorial
+ */
+
+var gulp = require('gulp'),
+    gulputil = require('gulp-util');
+    
+gulp.task('log',function(){
+    gulputil.log("this is my first log")
+});
+
