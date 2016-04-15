9697b0f83bd6d2354f9e410259e34cf5dd7cebbe
Anurup <Anurup>
2016-04-15 12:18:39 +0800

    JS Minification Tutorial

--- gulpfile.js
+++ gulpfile.js
@@ -7,19 +7,38 @@
 
 var gulp = require('gulp'),
     gulputil = require('gulp-util'),
-    gulpcoffee = require('gulp-coffee');
+    gulpcoffee = require('gulp-coffee'),
+    concat = require('gulp-concat');
 
 //Assign Variable
-var components ="components/",
-    coffee = components + "coffee/",
-    coffeeSources = [coffee + 'tagline.coffee'],
-    coffeeAll = coffee + "*.coffee",
-    saas =  components + "sass/",
-    js = components + "scripts/",
-    build = "builds/",
+var build = "builds/",
     source = build + "development/",
-    dest =  build + "production/";
+    dest =  build + "production/",
+    components = "components/",
+    coffeePath =  components + "coffee/",
+    jsPath = components + "scripts/",
+    coffee = { 
+        in : coffeePath,
+        coffeeAll : coffeePath + "*.coffee",
+        coffeeSources : [coffeePath + 'tagline.coffee'] 
+    },
+    saas =  components + "sass/",
+    js = {
+        in : jsPath,
+        source : source + "js/",
+        dest : dest + "js/",
+        jsSources : [
+                        jsPath + "rclick.js",
+                        jsPath + "pixgrid.js",
+                        jsPath + "tagline.js",
+                        jsPath + "template.js"
+                      ]
+        };
 
 gulp.task('coffee',function(){
-    gulp.src(coffeeSources).pipe(gulpcoffee({ bare : true }).on('error',gulputil.log)).pipe(gulp.dest(js));
+    gulp.src(coffee.coffeeSources).pipe(gulpcoffee({ bare : true }).on('error',gulputil.log)).pipe(gulp.dest(js.in));
 })
+
+gulp.task('js',function(){
+    gulp.src(js.jsSources).pipe(concat('script.js')).pipe(gulp.dest(js.source));
+});
